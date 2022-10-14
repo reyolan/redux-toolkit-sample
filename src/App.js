f485b42329cb4d99576ea186f72d4be30bc784f9
reyolan <reynardolan@gmail.com>
2022-10-14 16:59:22 +0800

    finish 3/4 of part 4

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import {
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
+import { PostsList } from './components/posts/PostsList'
+import { AddPostForm } from './components/posts/AddPostForm'
+import { SinglePostPage } from './components/posts/SinglePostPage'
+import { EditPostForm } from './components/posts/EditPostForm'
 
 function App() {
   return (
@@ -18,11 +22,14 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <>
+                <AddPostForm />
+                <PostsList />
+              </>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
